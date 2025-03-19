fc372abf753e96f184d41743fa96885d45def9d7
Kunal Mandave <mandavekunal@gmail.com>
2025-03-19 14:56:07 +0530

    Refactor UserServiceTests: disable tests for demo purposes and clean up code

--- src/test/java/com/kunal/journalApp/service/UserServiceTests.java
+++ src/test/java/com/kunal/journalApp/service/UserServiceTests.java
@@ -15,10 +15,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
-    public class UserServiceTests {
+@Disabled("Demo purposes")
+public class UserServiceTests {
 
     @Autowired
     private UserService userService;
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After all");
+    }
 
     @Test
     public void test() {
@@ -28,52 +39,38 @@ import static org.junit.jupiter.api.Assertions.*;
     @ParameterizedTest
     @ValueSource(strings = {
             "admin",
-            "admin1",
+//            "admin1",
             "admin2",
     })
     public void findUserByUsername(String username) {
         assertNotNull(userService.findByUsername(username), "User not found");
     }
 
-    @ParameterizedTest
-   @ArgumentsSource(UserArgumentProvider.class)
-    public void createNewUserTest(Users user) {
-        assertTrue(userService.saveNewUser(user),"Error while creating new user");
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {
-            "1, 2, 3",
-            "2, 3, 5",
-            "3, 4, 10"
-    })
+//    @ParameterizedTest
+//    @ArgumentsSource(UserArgumentProvider.class)
+//    public void createNewUserTest(Users user) {
+//        assertTrue(userService.saveNewUser(user), "Error while creating new user");
+//    }
+//
+//    @ParameterizedTest
+//    @CsvSource(value = {
+//            "1, 2, 3",
+//            "2, 3, 5",
+//            "3, 4, 7"
+//    })
     public void test(int a, int b, int result) {
         assertEquals(result, a + b, "The result is not correct for " + a + " + " + b + " = " + result);
     }
-
 
     @BeforeEach
     public void beforeEach() {
         System.out.println("Before each");
     }
 
-    @BeforeAll
-    public static void beforeAll() {
-        System.out.println("Before all");
-    }
-
     @AfterEach
     public void afterEach() {
         System.out.println("After each");
     }
-
-    @AfterAll
-    public static void afterAll() {
-        System.out.println("After all");
-    }
-
-
-
 
 
 }
